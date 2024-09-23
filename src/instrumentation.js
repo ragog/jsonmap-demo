924215d625f7e8b3153175f4097621d2605b3fb0
ragog <giovanni@checklyhq.com>
2024-09-23 11:11:30 +0200

    feat: expand instrumentation for collector

--- src/instrumentation.js
+++ src/instrumentation.js
@@ -3,6 +3,10 @@ const { OTLPTraceExporter } = require('@opentelemetry/exporter-trace-otlp-proto'
 const { getNodeAutoInstrumentations } = require('@opentelemetry/auto-instrumentations-node')
 const { BatchSpanProcessor, SamplingDecision } = require('@opentelemetry/sdk-trace-base')
 const { trace } = require('@opentelemetry/api')
+const { Resource } = require('@opentelemetry/resources')
+const { SEMRESATTRS_SERVICE_NAME, SEMRESATTRS_SERVICE_VERSION, } = require('@opentelemetry/semantic-conventions')
+const { OTLPMetricExporter } = require('@opentelemetry/exporter-metrics-otlp-proto');
+const { PeriodicExportingMetricReader } = require('@opentelemetry/sdk-metrics');
 
 const exporter = new OTLPTraceExporter({
   timeoutMillis: 2000,
@@ -34,6 +38,18 @@ const sdk = new NodeSDK({
       }
     },
   },
+  resource: new Resource({
+    [SEMRESATTRS_SERVICE_NAME]: 'dice-server',
+    [SEMRESATTRS_SERVICE_VERSION]: '0.1.0',
+  }),
+  traceExporter: new OTLPTraceExporter({
+    url: `http://${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/traces`
+  }),
+  metricReader: new PeriodicExportingMetricReader({
+    exporter: new OTLPMetricExporter({
+        url: `http://${process.env.OTEL_EXPORTER_OTLP_ENDPOINT}/v1/metrics`
+      }),
+  })
 })
 
 sdk.start()
